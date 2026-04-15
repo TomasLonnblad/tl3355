edf69c9a51a5efe0036950992fcca4a264756716
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-15 14:54:51 +0200

    x

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'pbs.twimg.com',
+
       },
       {
         protocol: 'https',
-        hostname: 'storage.googleapis.com',
+     
       },
     ],
   },
