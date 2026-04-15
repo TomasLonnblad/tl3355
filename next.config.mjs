6bf054006a287914741172a9e475d7280db74b66
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-15 15:19:58 +0200

    x

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+
+    ],
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
